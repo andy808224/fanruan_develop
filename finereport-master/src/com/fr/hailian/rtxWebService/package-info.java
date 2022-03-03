832988cacdc5434de8e7fe0f2c8f9c478e06aa5d
andy808224 <41136055+andy808224@users.noreply.github.com>
2022-03-03 20:14:48 +0800

    Delete finereport-master directory

--- finereport-master/src/com/fr/hailian/rtxWebService/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.nwh.cn/Rpc", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.fr.hailian.rtxWebService;
--- finereport-master/src/com/fr/hailian/ssoWebService/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://sso.nwh.cn", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.fr.hailian.ssoWebService;
